97619f809ea9f435ab90637d0a155c125a2fb84e
samocrutyk <tostglopiktost1@gmail.com>
2023-11-23 19:46:56 +0200

    Initial commit

---
+++ laba_slownyk.py
@@ -0,0 +1,15 @@
+order={}
+money=0
+print('ви прийшли у ресторан і готові віддати все щоб добре поїсти')
+order['імя']=input('імя:\n')
+age=int(input('ваш вік:\n'))
+order['сніданок']=input('головне:\n')
+if age >= 4:
+    order['обід']=input('друге:\n')
+order['вечерю']=input('десерт:\n')
+print('всього з вас:')
+for i in order:
+     money=money+len(order[i])
+print(money , 'грн')
+order['вік'] = age
+print(order)
