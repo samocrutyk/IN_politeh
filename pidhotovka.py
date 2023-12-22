037a1f97cf230d52f29d67bca332ec2b3ded97ef
samocrutyk <tostglopiktost1@gmail.com>
2023-12-22 07:36:19 +0200

    Create pidhotovka.py

---
+++ pidhotovka.py
@@ -0,0 +1,42 @@
+#   просто сортування і забирання повторення
+# n = [1, 7, 43]
+# new_n = []
+# for i in n:
+#     if i not in new_n:
+#         new_n.append(i)
+# new_nn= new_n.sort()
+# print(new_n)
+#       парні та непарні числа
+# n = [12,32,54,34,23]
+# nn=[]
+# nnn=[]
+# for i in n:
+#     if i % 2 == 0:
+#         nn.append(i)
+#     elif i % 2 == 1:
+#         nnn.append(i)
+# print(nn)
+# print(nnn)
+#         пробував перемішати 2 слова
+# n, f = "Oleg", "Artem"
+# new=[]
+# for leter in n,f:
+#     new.append(leter)
+# print(new)
+#       вибрати букву
+# leterss = ['d']
+# text = input(str("текст: "))
+# text = text.lower()
+# numbers = []
+# for leter in leterss:
+#     numbers.append(text.count(leter))
+# for i in range(len(leterss)):
+#     print(f"{leterss[i]}:{numbers[i]}")
+#  забираю зайві пробіли
+# text = input(str("текст: "))
+# textspl=text.split()
+# textres=''
+# for i in textspl:
+#     textres=textres+i+' '
+# print(textspl)
+# print(textres)
